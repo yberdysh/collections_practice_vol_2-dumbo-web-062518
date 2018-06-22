c50c972237debeb9b5ef2888604095e91376a57c
Jane Costa <yberdysh@gmail.com>
2018-06-22 16:38:11 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,4 +1,4 @@
-
+require "pry"
 # your code goes here
 def begins_with_r(arr)
 	arr.all?{|elem| elem[0].downcase == "r"}
@@ -25,23 +25,54 @@ def count_elements(arr)
 		elem[:count] = counter
 		final_array << elem
 	end
-	final_array.uniq
+	final_array
 end
 
-# def merge_data(keys, data)
-# 	new_arr = []
-# 	keys.map do |item|
-# 		data.map do |datum|
-# 			new_obj = {:first_name }
-# 		end
-# 	end
-# 	new_arr
-# end
+
+def count_elements(array)
+  array.each do |original_hash|
+    original_hash[:count] = 0
+    name = original_hash[:name]
+    array.each do |hash|
+      if hash[:name] == name
+        original_hash[:count] += 1
+      end
+    end
+  end.uniq
+end
+
+
+def merge_data(keys, values)
+  container = []
+  keys.each do |person_name|
+    name = person_name[:first_name]
+    values.each do |person_data|
+      if person_data[name]
+        merged_person = person_data[name]
+        merged_person[:first_name] = name
+        container << merged_person
+      end
+    end
+  end
+  container
+end
 
 def find_cool(arr)
 	arr.find_all{|item| item[:temperature] == "cool"}
 end
 
-def organize_schools(arr)
-	
+def organize_schools(schools)
+  organized_schools = {}
+  binding.pry
+  schools.each do |name, location_hash|
+    location = location_hash[:location]
+    if organized_schools[location]
+      organized_schools[location] << name
+    else
+      organized_schools[location] = []
+      organized_schools[location] << name
+    end
+  end
+  organized_schools
 end
+
